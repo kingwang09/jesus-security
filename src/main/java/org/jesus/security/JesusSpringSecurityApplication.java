c395f9b3bc6bf8eefded3b58d1c8b92acd1e4c9c
hyungeun <kingwang09@gmail.com>
2023-01-31 13:42:48 +0900

    spring-security 초기 예제

--- src/main/java/org/jesus/security/JesusSpringSecurityApplication.java
+++ src/main/java/org/jesus/security/JesusSpringSecurityApplication.java
@@ -1,4 +1,4 @@
-package org.jesus.security.jesusspringsecurity;
+package org.jesus.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
