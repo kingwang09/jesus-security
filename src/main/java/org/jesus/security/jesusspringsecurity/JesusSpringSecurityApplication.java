e7564e2b4b1cca3eaaa7b73d5da7439021a9c94f
hyungeun <kingwang09@gmail.com>
2023-01-31 13:30:59 +0900

    spring-security 예제 초기 구성

---
+++ src/main/java/org/jesus/security/jesusspringsecurity/JesusSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.jesus.security.jesusspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JesusSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JesusSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jesus/security/jesusspringsecurity/JesusSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jesus.security.jesusspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JesusSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
