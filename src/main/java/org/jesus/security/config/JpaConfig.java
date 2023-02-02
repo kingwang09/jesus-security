feb7b82a4d092a71d9d0acebf5e475c0f10f931d
hyungeun <kingwang09@gmail.com>
2023-02-02 15:07:45 +0900

    [#1] JPA Configuration 최소 설정

---
+++ src/main/java/org/jesus/security/config/JpaConfig.java
@@ -0,0 +1,17 @@
+package org.jesus.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = {
+                "org.jesus.security.domain.*.repository"
+        }
+)
+@EnableJpaAuditing
+@EnableTransactionManagement
+public class JpaConfig {
+}
