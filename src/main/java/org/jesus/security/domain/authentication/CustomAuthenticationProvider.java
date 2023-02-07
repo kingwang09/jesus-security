9f4f4e0d8dc721172ba80647a614d9f1c3fdddd2
hyungeun <kingwang09@gmail.com>
2023-02-07 16:00:59 +0900

    [#5] CustomAuthentication에 대한 인증 로직 구현

--- src/main/java/org/jesus/security/domain/authentication/CustomAuthenticationProvider.java
+++ src/main/java/org/jesus/security/domain/authentication/CustomAuthenticationProvider.java
@@ -3,8 +3,11 @@ package org.jesus.security.domain.authentication;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -22,11 +25,23 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         String userName = authentication.getName();
         String password = authentication.getCredentials().toString();
         log.debug("try login username={}, credential={}", userName, password);
-        return null;
+
+        //로그인 시도할 때마다 DB조회 -> redis로 바꾸긴 해야할듯
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userName);//not-null 보장
+
+//        if(passwordEncoder.matches(passwordEncoder.encode(password), userDetails.getPassword())){
+//
+//        }
+
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {//rawPassword를 사용. (encode된 rawPassword가 아님)
+            return new UsernamePasswordAuthenticationToken(userName, password, userDetails.getAuthorities());
+        } else {
+            throw new BadCredentialsException("Something went wrong!");
+        }
     }
 
     @Override
-    public boolean supports(Class<?> authentication) {
-        return false;
+    public boolean supports(Class<?> authenticationType) {
+        return authenticationType.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
