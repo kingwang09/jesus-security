2b0dd8316e7d20495237214e961e76b497a1eec6
hyungeun <kingwang09@gmail.com>
2023-02-10 14:35:01 +0900

    [#5] httpBasic & formLogin 모두 지원되도록 셋팅

--- src/main/java/org/jesus/security/config/SecurityAuthorizationConfig.java
+++ src/main/java/org/jesus/security/config/SecurityAuthorizationConfig.java
@@ -15,7 +15,10 @@ public class SecurityAuthorizationConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
+        http
+                .httpBasic()
+            .and()
+                .formLogin();
         http.authorizeRequests().anyRequest().authenticated();//모든 요청 권한 체크
         //http.authorizeRequests().anyRequest().permitAll();//모든 요청 권한 체크 안함
     }
