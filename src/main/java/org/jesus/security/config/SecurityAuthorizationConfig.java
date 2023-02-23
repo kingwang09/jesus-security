c5db713e595c9ec361b4d7286f56567063bca055
hyungeun <kingwang09@gmail.com>
2023-02-23 17:29:01 +0900

    Too busy configuration...

--- src/main/java/org/jesus/security/config/SecurityAuthorizationConfig.java
+++ src/main/java/org/jesus/security/config/SecurityAuthorizationConfig.java
@@ -16,9 +16,9 @@ public class SecurityAuthorizationConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic()
+            .httpBasic()
             .and()
-                .formLogin();
+            .formLogin();
         http.authorizeRequests().anyRequest().authenticated();//모든 요청 권한 체크
         //http.authorizeRequests().anyRequest().permitAll();//모든 요청 권한 체크 안함
     }
