0ed82c860232e39fbf3d67631765598ff45ed145
hyungeun.jin <kingwang09@gmail.com>
2023-05-09 13:44:15 +0900

    hasAnyAuthority & hasRole의 차이에 대해 알아봐야할 듯.

--- src/main/java/org/jesus/security/config/SecurityAuthorizationConfig.java
+++ src/main/java/org/jesus/security/config/SecurityAuthorizationConfig.java
@@ -1,6 +1,7 @@
 package org.jesus.security.config;
 
 import lombok.RequiredArgsConstructor;
+import org.jesus.security.domain.user.constant.UserRole;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -19,8 +20,12 @@ public class SecurityAuthorizationConfig extends WebSecurityConfigurerAdapter {
             .httpBasic()
             .and()
             .formLogin();
-        http.authorizeRequests().anyRequest().authenticated();//모든 요청 권한 체크
+        //http.authorizeRequests().anyRequest().authenticated();//모든 요청 권한 체크
         //http.authorizeRequests().anyRequest().permitAll();//모든 요청 권한 체크 안함
+
+
+        http.authorizeRequests().anyRequest().hasAnyAuthority(UserRole.USER.toString(), UserRole.ADMIN.toString());//허용됨.
+        //http.authorizeRequests().anyRequest().hasAnyRole(UserRole.USER.toString(), UserRole.ADMIN.toString());//거부됨.
     }
 
     /**
